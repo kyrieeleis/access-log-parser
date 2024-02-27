03c936353815a22bcea9c1fa6aba5f1558740524
Danil Kornilov <151754329+kyrieeleis@users.noreply.github.com>
2024-02-28 00:09:25 +0300

    quotient fixed, difference changes discarded

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ public class Main {
         System.out.println("Введите второе число:");
         int secondnumber = new Scanner(System.in).nextInt();
         int sum = firstnumber + secondnumber;
-        int difference = secondnumber-firstnumber;
-        int product = firstnumber*secondnumber;
-        double quotient = firstnumber/secondnumber;
+        int difference = firstnumber - secondnumber;
+        int product = firstnumber * secondnumber;
+        double quotient = firstnumber % secondnumber;
         System.out.println("Сумма введенных чисел: " + sum);
         System.out.println("Произведение введенных чисел: " + product);
         System.out.println("Разность введенных чисел: " + difference);
