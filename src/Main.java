268af82725e5ec40588b15c06c8856f0a43c7b8c
Danil Kornilov <d.kornilov3003@gmail.com>
2024-03-05 19:38:31 +0300

    filepath validation added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int counter = 1;
+        for (; ; ) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExtists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (fileExtists == false || isDirectory == true) {
+                System.out.println("Chosen just directory (no file) or selected file doesn't exists. Please select a valid file or path.");
+                continue;
+            }
+            if (fileExtists == true) {
+                System.out.println("Gotcha! " + "This is valid file number " + counter + ".");
+                counter++;
+            }
+        }
     }
 }
+
