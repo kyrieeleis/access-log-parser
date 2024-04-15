f3947d61f7240131e449047f31775bd8543b7935
Danil Kornilov <d.kornilov3003@gmail.com>
2024-04-15 22:19:36 +0300

    Functional of parsing added: total, max and min length of line, LineTooLongException added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -7,16 +8,42 @@ public class Main {
         for (; ; ) {
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
-            boolean fileExtists = file.exists();
+            boolean fileExists = file.exists();
             boolean isDirectory = file.isDirectory();
 
-            if (fileExtists == false || isDirectory == true) {
+            if (fileExists == false || isDirectory == true) {
                 System.out.println("Chosen just directory (no file) or selected file doesn't exists. Please select a valid file or path.");
                 continue;
             }
-            if (fileExtists == true) {
-                System.out.println("Gotcha! " + "This is valid file number " + counter + ".");
+            if (fileExists) {
+                System.out.println("Correct. " + "This is valid file number " + counter + ".");
                 counter++;
+                int minLength = Integer.MAX_VALUE;
+                int maxLength = Integer.MIN_VALUE;
+                int lineCounter = 0;
+                String line;
+                try {
+                    FileReader fileReader = new FileReader(path);
+                    BufferedReader reader = new BufferedReader(fileReader);
+                    while ((line = reader.readLine()) != null) {
+                        int length = line.length();
+                        if (line.length() > 1024) {
+                            throw new LineTooLongException("Line too long (1024 characters max).");
+                        }
+                        if (line.length() < minLength) {
+                            minLength = line.length();
+                        } else if (line.length() >= maxLength) {
+                            maxLength = line.length();
+                        }
+                        lineCounter++;
+                    }
+                    System.out.println("Total number of lines: " + lineCounter);
+                    System.out.println("Min length: " + minLength);
+                    System.out.println("Max length: " + maxLength);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
             }
         }
     }
