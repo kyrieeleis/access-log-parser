ed063f5ca552dba40f874d967925b2f4402b5cc9
Danil Kornilov <151754329+kyrieeleis@users.noreply.github.com>
2024-04-16 22:27:52 +0300

    LogEntry added

---
+++ src/LogEntry.java
@@ -0,0 +1,56 @@
+import java.time.LocalDateTime;
+
+public class LogEntry {
+    private final String ipAddress;
+    private final LocalDateTime dateTime;
+    private final String httpMethod;
+    private final String path;
+    private final int statusCode;
+    private final int bytesSent;
+    private final String referer;
+    private final String userAgent;
+
+    public LogEntry(String line){
+        String[] fields = line.split(" ");
+        this.ipAddress = fields[0];
+        this.dateTime = LocalDateTime.parse(fields[3]);
+        this.httpMethod = fields[5].replace("\"", "");
+        this.path = fields[6].replace("\"", "");
+        this.statusCode = Integer.parseInt(fields[8]);
+        this.bytesSent = Integer.parseInt(fields[9]);
+        this.referer = fields[10].replace("\"", "");
+        this.userAgent = fields[12].replace("\"", "");
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getHttpMethod() {
+        return httpMethod;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public int getBytesSent() {
+        return bytesSent;
+    }
+
+    public String getReferer() {
+        return referer;
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
+}
