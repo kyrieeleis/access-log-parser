35179f04cd8784d405754d73815d315530ba66ef
Danil Kornilov <151754329+kyrieeleis@users.noreply.github.com>
2024-04-16 21:50:39 +0300

    YandexBot and Googlebot parsing added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -12,16 +13,17 @@ public class Main {
             boolean isDirectory = file.isDirectory();
 
             if (fileExists == false || isDirectory == true) {
-                System.out.println("Chosen just directory (no file) or selected file doesn't exists. Please select a valid file or path.");
+                System.out.println("Chosen just directory (not file) or selected file doesn't exists. Please select a valid file or path.");
                 continue;
             }
             if (fileExists) {
                 System.out.println("Correct. " + "This is valid file number " + counter + ".");
                 counter++;
-                int minLength = Integer.MAX_VALUE;
-                int maxLength = Integer.MIN_VALUE;
                 int lineCounter = 0;
+                int yandexCounter = 0;
+                int googleCounter = 0;
                 String line;
+                // String[] parts = new String[0];
                 try {
                     FileReader fileReader = new FileReader(path);
                     BufferedReader reader = new BufferedReader(fileReader);
@@ -30,22 +32,40 @@ public class Main {
                         if (line.length() > 1024) {
                             throw new LineTooLongException("Line too long (1024 characters max).");
                         }
-                        if (line.length() < minLength) {
-                            minLength = line.length();
-                        } else if (line.length() >= maxLength) {
-                            maxLength = line.length();
+                        String[] firstBrackets = line.split("\"");
+                        String parts[] = firstBrackets[5].split(";");
+                        String fragment = null;
+                        if (parts.length >= 2) {
+                            fragment = parts[1];
+                            fragment.split("/");
+                            for (String part : parts) {
+                                if (part.contains("YandexBot")) {
+                                    part.trim();
+                                    yandexCounter++;
+                                }
+
+                                if (part.contains("Googlebot")) {
+                                    part.trim();
+                                    googleCounter++;
+                                }
+                            }
                         }
                         lineCounter++;
                     }
                     System.out.println("Total number of lines: " + lineCounter);
-                    System.out.println("Min length: " + minLength);
-                    System.out.println("Max length: " + maxLength);
+                    System.out.println("YandexBot: " + yandexCounter + " or " + Math.round(percentage(yandexCounter, lineCounter)) + "% of total.");
+                    System.out.println("Googlebot: " + googleCounter + " or " + Math.round(percentage(googleCounter, lineCounter)) + "% of total.");
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
-
             }
+
         }
     }
+
+    private static double percentage(int a, int b) {
+        return (double) a / (double) b * 100;
+    }
 }
+
 
