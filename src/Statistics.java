6633088920717c5e4cf35ed6b0eeb36699820875
Danil Kornilov <151754329+kyrieeleis@users.noreply.github.com>
2024-04-18 21:45:25 +0300

    Statistics realisation added

---
+++ src/Statistics.java
@@ -0,0 +1,30 @@
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Statistics {
+    private int totalTraffic;
+    private LocalDateTime minTime;
+    private LocalDateTime maxTime;
+
+    public Statistics(){
+    }
+
+
+    public void addEntry(LogEntry o){
+        this.totalTraffic += o.getBytesSent();
+        if(minTime == null || o.getDateTime().isBefore(minTime)){
+            this.minTime = o.getDateTime();
+        }
+        if(maxTime == null || o.getDateTime().isAfter(maxTime)){
+            this.maxTime = o.getDateTime();
+        }
+    }
+    public int getTrafficRate(){
+        int difference = (int) minTime.until(maxTime, ChronoUnit.HOURS);
+        if (difference == 0){
+            difference = 1;
+        }
+        return totalTraffic / difference;
+    }
+
+}
