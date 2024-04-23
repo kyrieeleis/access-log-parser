18fc91fceb2b5b054456082c9aa25c9ff0274ee9
Danil Kornilov <151754329+kyrieeleis@users.noreply.github.com>
2024-04-23 22:49:03 +0300

    getUniquePaths method added, new class field and initialization added

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,34 +1,48 @@
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Statistics {
     private int totalTraffic;
     private LocalDateTime minTime;
     private LocalDateTime maxTime;
+    private HashSet<String> uniquePaths;
 
-    public Statistics(){
+    public Statistics() {
         this.minTime = null;
         this.maxTime = null;
         this.totalTraffic = 0;
     }
 
 
-    public void addEntry(LogEntry o){
+    public void addEntry(LogEntry o) {
         this.totalTraffic += o.getBytesSent();
-        if(minTime == null || o.getDateTime().isBefore(minTime)){
+        if (minTime == null || o.getDateTime().isBefore(minTime)) {
             this.minTime = o.getDateTime();
         }
-        if(maxTime == null || o.getDateTime().isAfter(maxTime)){
+        if (maxTime == null || o.getDateTime().isAfter(maxTime)) {
             this.maxTime = o.getDateTime();
         }
+        if (o.getStatusCode() == 200) {
+            uniquePaths.add(o.getPath());
+        }
     }
-    public int getTrafficRate(){
+
+    public HashSet<String> getUniquePaths() {
+        return new HashSet<>(new HashSet<>(uniquePaths));
+    }
+
+    public int getTrafficRate() {
         int difference = (int) minTime.until(maxTime, ChronoUnit.HOURS);
-        if (difference == 0){
+        if (difference == 0) {
             difference = 1;
         }
         return totalTraffic / difference;
     }
-    publ
 
 }
+
+
