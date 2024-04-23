c358df149c7f981164bb0ba130e6cca274efae78
Danil Kornilov <151754329+kyrieeleis@users.noreply.github.com>
2024-04-23 22:26:40 +0300

    UserAgent refactored

--- src/UserAgent.java
+++ src/UserAgent.java
@@ -4,58 +4,56 @@ import static java.util.Collections.replaceAll;
 
 
 public class UserAgent {
-    private String operatingSystem;
-    private String browser;
+    public String operatingSystem;
+    public String browser;
 
     public UserAgent(String userAgent) {
-        String[] parts = userAgent.split("\"");
-        if (parts.length >= 2) {
-            String fragment = parts[5];
-            String[] fragments = fragment.split(" ");
-            String[] os = fragments[1].split(";");
-            if (os[1].contains("Windows")) {
-                this.operatingSystem = OperatingSystem.WINDOWS.toString();
-            } else if (os[1].contains("Mac")) {
-                this.operatingSystem = OperatingSystem.MAC.toString();
-            } else if (os[1].contains("Linux")) {
-                this.operatingSystem = OperatingSystem.LINUX.toString();
-            } else if (os[1].contains("Android")) {
-                this.operatingSystem = OperatingSystem.ANDROID.toString();
-            } else if (os[1].contains("iOS")) {
-                this.operatingSystem = OperatingSystem.IOS.toString();
-            } else {
-                this.operatingSystem = OperatingSystem.OTHER.toString();
-
-            }
-            this.operatingSystem = os[0].trim();
-
-            if (fragments[fragments.length - 1].contains("Chrome")) {
-                this.browser = Browser.CHROME.toString();
-            } else if (fragments[2].contains("Edge")) {
-                this.browser = Browser.EDGE.toString();
-            } else if (fragments[2].contains("Safari")) {
-                this.browser = Browser.SAFARI.toString();
-            } else if (fragments[2].contains("Opera")) {
-                this.browser = Browser.OPERA.toString();
-            } else if (fragments[2].contains("Firefox")) {
-                this.browser = Browser.FIREFOX.toString();
-            } else
-                this.browser = Browser.OTHER.toString();
-
-
+        String[] parts = userAgent.split(";");
+        if (parts[1].contains("Windows") || parts[1].contains("Win") || parts[1].contains("Win32") || parts[1].contains("Win64") || parts[1].contains("Win64; x64") || parts[1].contains("WOW64")) {
+            this.operatingSystem = OperatingSystem.WINDOWS.toString();
+        } else if (parts[1].contains("Mac") || parts[2].contains("MacOS") || parts[2].contains("iOS")) {
+            this.operatingSystem = OperatingSystem.MAC.toString();
+        } else if (parts[1].contains("Linux")) {
+            this.operatingSystem = OperatingSystem.LINUX.toString();
+        } else if (parts[1].contains("Android")) {
+            this.operatingSystem = OperatingSystem.ANDROID.toString();
+        } else if (parts[1].contains("iOS")) {
+            this.operatingSystem = OperatingSystem.IOS.toString();
+        } else {
+            this.operatingSystem = OperatingSystem.OTHER.toString();
         }
+        String[] browserFragment = parts[parts.length - 1].split("/");
+        for (String s : browserFragment) {
+            if (s.contains("Chrome")) {
+                this.browser = Browser.CHROME.toString();
+                break;
+            } else if (s.contains("Edge")) {
+                this.browser = Browser.EDGE.toString();
+                break;
+            } else if (s.contains("Safari")) {
+                this.browser = Browser.SAFARI.toString();
+                break;
+            } else if (s.contains("Opera")) {
+                this.browser = Browser.OPERA.toString();
+                break;
+            } else if (s.contains("Firefox")) {
+                this.browser = Browser.FIREFOX.toString();
+                break;
+            }
+            this.browser = Browser.OTHER.toString();
+        }
+
     }
 }
 
 enum Browser {
-    CHROME("Chrome"), EDGE("Edge"), SAFARI("Safari"), OPERA("Opera"), FIREFOX("Firefox"), OTHER("Other");
+    CHROME("Google Chrome"), EDGE("Microsoft Edge"), SAFARI("Safari"), OPERA("Opera"), FIREFOX("Mozilla Firefox"), OTHER("Other browser");
 
     private String title;
 
     Browser(String title) {
         this.title = title;
     }
-
 
     @Override
     public String toString() {
@@ -64,7 +62,7 @@ enum Browser {
 }
 
 enum OperatingSystem {
-    WINDOWS("Windows"), MAC("Mac"), LINUX("Linux"), ANDROID("Android"), IOS("iOS"), OTHER("Other");
+    WINDOWS("Windows"), MAC("MacOS (or other MacOS-like OS)"), LINUX("Linux"), ANDROID("Android"), IOS("iOS"), OTHER("Other OS");
 
     private String title;
 
@@ -76,7 +74,6 @@ enum OperatingSystem {
     public String toString() {
         return title;
     }
-
 }
 
 
